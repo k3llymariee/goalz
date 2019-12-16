c45d1965f16a3d7a3e42608743b30ef6a34bcd5c
Kelly Hofmann <kellymhofmann@gmail.com>
2019-12-15 17:23:18 -0800

    Added the ability to add new goals

---
+++ model.py
@@ -0,0 +1,57 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_to_db(app, db_uri="postgresql:///goals"):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our PostgreSQL database
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # app.config['SQLALCHEMY_ECHO'] = True
+    db.app = app
+    db.init_app(app)
+
+
+class User(db.Model):
+    """Users in the system"""
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+
+    goals = db.relationship('Goal', 
+                             backref='users')
+    def __repr__(self):
+        return f'<User id={self.id} email={self.email}>'
+
+class Goal(db.Model):
+    """Goals added to the database by users"""
+
+    __tablename__ = 'goals'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    description = db.Column(db.Text)
+
+
+    def __repr__():
+        return f'<Goal id={self.id}>'
+
+
+def init_app():
+    from flask import Flask
+    app = Flask(__name__)
+
+    connect_to_db(app)
+    print("Connected to DB")
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    from server import app
+    init_app()
--- server.py
+++ server.py
@@ -1,20 +1,116 @@
-from flask import Flask, render_template
+from jinja2 import StrictUndefined
+from flask import Flask, render_template, request, flash, redirect, session, jsonify
+from flask_debugtoolbar import DebugToolbarExtension
+from model import *
 
-# Create the application instance
 app = Flask(__name__)
 
-# Create a URL route mainpage
-@app.route('/')
-def home():
-    """
-    This function responds to the browser ULR localhost:5000/
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "super-secret"
 
-    :return:        the rendered template 'home.html'
-    """
+# raise an error in Jinja2 when using an undefined variable
+app.jinja_env.undefined = StrictUndefined
+
+debug = '\n' * 3
+
+
+@app.route('/')
+def index():
+    """Defaults user view to today's log entries"""
+
     return render_template('home.html')
 
-# If we're running in stand alone mode, run the application
+
+@app.route('/register', methods=['GET'])
+def register_form():
+    """Show form for user signup"""
+
+    return render_template('login.html')
+
+
+@app.route('/register', methods=['POST'])
+def register_process():
+    """Create new user in the database"""
+
+    register_form = request.form 
+
+    if User.query.filter(User.email == register_form['email']).first():
+        flash('Email already exists within our userbase')
+        return redirect('/register')
+
+    elif register_form['password'] != register_form['confirm_password']:
+        flash('Passwords do not match')
+        return redirect('/register')
+
+    else:
+        new_user = User(email=register_form['email'],
+                        password=register_form['password'])
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Added new user')
+
+        session['user_id'] = new_user.id
+        flash('Successfully logged in')
+        return redirect('/')
+
+
+@app.route('/login', methods=['POST'])
+def process_login():
+    """Log a user in if the user is in the database and provides correct password"""
+
+    login_attempt = request.form
+
+    user = User.query.filter(User.email == login_attempt['email']).first()
+
+    # check if a user exists in the database
+    if not user:
+        flash('Incorrect email or password')
+        return redirect("/login")
+
+    # check if their password matches
+    elif user.password != login_attempt['password']:
+        flash('Incorrect password or email')
+        return redirect("/login")
+
+    # if yes to both above, add user_id to session data
+    else:
+        session['user_id'] = user.id
+        flash('Successfully logged in')
+        return redirect('/')
+
+
+@app.route('/logout')
+def process_logout():
+    """Log a user out by deleting their session variable"""
+
+    del session['user_id']
+    flash('Successfully logged out')
+    return redirect("/register")
+
+
+@app.route('/api/goal', methods=['POST'])
+def add_goal():
+    """Adds a new goal to the database for the logged in user"""
+
+    user_id = session['user_id']
+    description = request.form.get('goal_description')
+
+    new_goal = Goal(user_id=user_id, description=description)
+    db.session.add(new_goal)
+    db.session.commit()
+
+    return "New goal has been added!"
+
+
 if __name__ == '__main__':
+
     # Remove debug for demo
     app.debug = True
+
+
+    connect_to_db(app)
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
     app.run(host='0.0.0.0')
